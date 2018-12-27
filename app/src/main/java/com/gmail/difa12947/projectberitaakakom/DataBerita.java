f5dddc4c200bc715784b6ee91a591adc2cb8867d
difais <difa12947@gmail.com>
2018-12-27 16:45:04 +0700

    tugas

---
+++ app/src/main/java/com/gmail/difa12947/projectberitaakakom/DataBerita.java
@@ -0,0 +1,8 @@
+package com.gmail.difa12947.projectberitaakakom;
+
+public class DataBerita {
+    public String judulBerita;
+    public String isiBerita;
+    public String tglPosting;
+    public String gambarBerita;
+}
